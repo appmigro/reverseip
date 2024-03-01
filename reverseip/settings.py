7115fa6e134e3e52a84ba687970791cbc8241f54
Akande Adedayo <akandeadedayo01@gmail.com>
2024-03-01 14:58:59 -0600

    Adding Health Check

--- reverseip/settings.py
+++ reverseip/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reverseapp',
+    'health_check',
 ]
 
 MIDDLEWARE = [
--- reverseip/urls.py
+++ reverseip/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reverseapp.urls')),
+    path('health_check/', include('health_check.urls')),
 ]
