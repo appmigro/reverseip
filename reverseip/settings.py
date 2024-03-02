1bf3b33d8bd74b39cefa48a92d9f048108b81c5c
Akande Adedayo <akandeadedayo01@gmail.com>
2024-03-02 02:14:45 -0600

    Making secret key blank

--- reverseip/settings.py
+++ reverseip/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-je1tm38uaw@)hs2t&xz-lx++f*e_nlen6*=x*=a^1)yv3=vu5p'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
